6dde8dd53cef461b1326497acd39dad51d632abd
Anthony Frey <anthonyfrey@Anthonys-MacBook-Pro.local>
2018-04-25 18:28:25 -0400

    First push

---
+++ src/main/java/com/mycompany/gametoolmaven/SceneController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gametoolmaven;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+/**
+ * FXML Controller class
+ *
+ * @author anthonyfrey
+ */
+public class SceneController implements Initializable {
+
+    @FXML
+    private MenuItem menuFileClose;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void menuFileCloseClicked(ActionEvent event) {
+        Platform.exit();
+    }
+    
+}
